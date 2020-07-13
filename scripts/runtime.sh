d9881a23f3cc70f2b4d2448adaab41849449daf4
buxiaomo <95112082@qq.com>
2020-07-13 18:45:20 +0800

    add update

--- scripts/runtime.sh
+++ scripts/runtime.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 command -v yum &> /dev/null && export PKG=yum
 command -v apt &> /dev/null && export PKG=apt
+${PKG} update -y
 ${PKG} install python3 python3-pip sshpass curl rsync -y
 pip3 install --upgrade -i https://mirrors.aliyun.com/pypi/simple/ "ansible==2.9.7"
 chmod +x /usr/local/bin/ansible*
