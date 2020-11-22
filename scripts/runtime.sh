d3e8c7db4363f1988667318f2ad14737c6702d72
buxiaomo <95112082@qq.com>
2020-11-22 22:36:19 +0800

    add docker daemon option update journald

--- scripts/runtime.sh
+++ scripts/runtime.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 command -v yum &> /dev/null && export PKG=yum
 command -v apt &> /dev/null && export PKG=apt
-${PKG} update -y
-${PKG} install python2 python2-pip sshpass curl rsync -y
+case ${PKG} in
+    'yum')
+        yum install -y epel-release
+        yum install python2 python2-pip sshpass curl rsync -y
+        ;;
+    'apt')
+        apt-get update
+        apt-get install python2 python2-pip sshpass curl rsync -y
+        ;;
+    '*')
+        echo "unknow OS version."
+        exit 1
+        ;;
+esac
 pip install --upgrade -i https://mirrors.aliyun.com/pypi/simple/ "ansible==2.9.7"
