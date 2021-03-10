584ea6383bfae78d7810558c6cc16372744c3589
buxiaomo <95112082@qq.com>
2021-03-11 00:12:30 +0800

    update ansible version

--- scripts/runtime.sh
+++ scripts/runtime.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
-command -v yum &> /dev/null && export PKG=yum
-command -v apt &> /dev/null && export PKG=apt
+command -v yum &>/dev/null && export PKG=yum
+command -v apt &>/dev/null && export PKG=apt
 case ${PKG} in
-    'yum')
-        yum install -y epel-release
-        yum install python3 python3-pip sshpass curl rsync -y
-        ;;
-    'apt')
-        apt-get update
-        apt-get install python3 python3-pip sshpass curl rsync -y
-        ;;
-    '*')
-        echo "unknow OS version."
-        exit 1
-        ;;
+'yum')
+    yum install -y epel-release
+    yum install python3 python3-pip sshpass curl rsync -y
+    ;;
+'apt')
+    apt-get update
+    apt-get install python3 python3-pip sshpass curl rsync -y
+    ;;
+'*')
+    echo "unknow OS version."
+    exit 1
+    ;;
 esac
 pip3 install --upgrade pip
-pip3 install --upgrade -i https://mirrors.aliyun.com/pypi/simple/ "ansible==2.10.7"
+pip3 install --upgrade -i https://mirrors.aliyun.com/pypi/simple/ "ansible==3.0.0"
