e081d0298f220e802ea22de281421d71ba3f5683
buxiaomo <95112082@qq.com>
2021-06-16 14:02:20 +0800

    update ali

--- scripts/runtime.sh
+++ scripts/runtime.sh
@@ -15,5 +15,5 @@ case ${PKG} in
     exit 1
     ;;
 esac
-pip3 install --upgrade pip
+pip3 install --upgrade -i https://mirrors.aliyun.com/pypi/simple/ pip
 pip3 install --upgrade -i https://mirrors.aliyun.com/pypi/simple/ "ansible==4.0.0"
