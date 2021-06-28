ba3a519e8b1de5c2a8627ee4421310a00838315e
buxiaomo <95112082@qq.com>
2021-06-28 21:36:17 +0800

    update formate

--- scripts/action.sh
+++ scripts/action.sh
@@ -3,7 +3,7 @@ set -x
 
 function kubernetes() {
   export tag_name=$(curl -s -k -f --connect-timeout 20 --retry 5 --location --insecure https://api.github.com/repos/kubernetes/kubernetes/releases/latest | jq -r .tag_name)
-  ls 
+  ls
   pwd
   grep -E "KUBE_VERSION.:=.${tag_name:1}" ./Makefile
   if [ $? -ne 0 ]; then
@@ -11,10 +11,9 @@ function kubernetes() {
     git config --local user.email "action@github.com"
     git config --local user.name "GitHub Action"
     git add ./Makefile
-    git commit -am "update kubernetes version to ${tag_name}"
+    git commit -am "aotu update kubernetes version to ${tag_name}"
     git push
   fi
 }
 
 $@
-
