7bca93e92efa3df6dbe33200aa3ac455517d5cbf
buxiaomo <95112082@qq.com>
2021-06-28 21:33:37 +0800

    fix

--- scripts/action.sh
+++ scripts/action.sh
@@ -3,10 +3,10 @@ set -ex
 
 function kubernetes() {
   export tag_name=$(curl -s -k -f --connect-timeout 20 --retry 5 --location --insecure https://api.github.com/repos/kubernetes/kubernetes/releases/latest | jq -r .tag_name)
+  ls 
+  pwd
   grep -E "KUBE_VERSION.:=.${tag_name:1}" ./Makefile
   if [ $? -ne 0 ]; then
-    pwd
-    ls -l
     sed -i "8s/KUBE_VERSION.*/KUBE_VERSION:=${tag_name:1}/" ./Makefile
     git config --local user.email "action@github.com"
     git config --local user.name "GitHub Action"
