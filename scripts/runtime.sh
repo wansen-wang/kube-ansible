51b886f4136f316c5b47eea9438ea2bbadd7254d
buxiaomo <95112082@qq.com>
2021-10-25 15:04:35 +0800

    update containerd config

--- scripts/runtime.sh
+++ scripts/runtime.sh
@@ -4,11 +4,11 @@ command -v apt &>/dev/null && export PKG=apt
 case ${PKG} in
 'yum')
     yum install -y epel-release
-    yum install python3 python3-pip sshpass curl rsync -y
+    yum install python3 python3-pip sshpass curl rsync wget -y
     ;;
 'apt')
     apt-get update
-    apt-get install python3 python3-pip sshpass curl rsync -y
+    apt-get install python3 python3-pip sshpass curl rsync wget -y
     ;;
 '*')
     echo "unknow OS version."
@@ -17,3 +17,6 @@ case ${PKG} in
 esac
 pip3 install --upgrade -i https://pypi.tuna.tsinghua.edu.cn/simple pip
 pip3 install --upgrade -i https://pypi.tuna.tsinghua.edu.cn/simple "ansible==4.7.0"
+
+wget https://github.com/mikefarah/yq/releases/download/v4.13.4/yq_linux_amd64 -O /usr/local/bin/yq
+chmod +x /usr/local/bin/yq
