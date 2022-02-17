5f2b6c8b98da53b81b9af3e32a88efd5b3093aaa
buxiaomo <95112082@qq.com>
2022-02-17 20:19:05 +0800

    update

--- scripts/info.sh
+++ scripts/info.sh
@@ -40,8 +40,8 @@ fi
 
 echo -e "CNI version: \t\t\t\033[32m${CNI_VERSION}\033[0m"
 
-if [ ! -n ${PKI_URL} ]; then
-  echo -e "PKI Url: \t\t\t${PKI_URL}"
+if [ -n ${PKI_URL} ]; then
+  echo -e "PKI Url: \t\t\t\033[32m${PKI_URL}\033[0m"
   ANSIBLE_ARG="${ANSIBLE_ARG} -e PKI_URL=${PKI_URL}"
 fi
 
