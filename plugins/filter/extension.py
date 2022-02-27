f9e4f486f5f6e458a8abd45955515386c6879947
buxiaomo <95112082@qq.com>
2022-02-27 16:41:49 +0800

    update

---
+++ plugins/filter/extension.py
@@ -0,0 +1,98 @@
+import re
+from ansible import errors
+import operator as py_operator
+from distutils.version import LooseVersion, StrictVersion
+from ansible.module_utils._text import to_native, to_text
+from ansible.utils.version import SemanticVersion
+
+
+def select(value, ipv4, ipv6):
+    if value == "ipv4":
+        return ipv4
+    else:
+        return "[%s]" % ipv6
+
+
+def split_string(string, separator=' '):
+    try:
+        return string.split(separator)
+    except Exception as e:
+        raise errors.AnsibleFilterError(
+            'split plugin error: %s, provided string: "%s"' % str(e), str(string))
+
+
+def split_regex(string, separator_pattern='\s+'):
+    try:
+        return re.split(separator_pattern, string)
+    except Exception as e:
+        raise errors.AnsibleFilterError(
+            'split plugin error: %s, provided string: "%s"' % str(e), str(string))
+
+
+def version_compare(value, version, operator='eq', strict=None, version_type=None):
+    ''' Perform a version comparison on a value '''
+    op_map = {
+        '==': 'eq', '=': 'eq', 'eq': 'eq',
+        '<': 'lt', 'lt': 'lt',
+        '<=': 'le', 'le': 'le',
+        '>': 'gt', 'gt': 'gt',
+        '>=': 'ge', 'ge': 'ge',
+        '!=': 'ne', '<>': 'ne', 'ne': 'ne'
+    }
+
+    type_map = {
+        'loose': LooseVersion,
+        'strict': StrictVersion,
+        'semver': SemanticVersion,
+        'semantic': SemanticVersion,
+    }
+
+    if strict is not None and version_type is not None:
+        raise errors.AnsibleFilterError(
+            "Cannot specify both 'strict' and 'version_type'")
+
+    if not value:
+        raise errors.AnsibleFilterError("Input version value cannot be empty")
+
+    if not version:
+        raise errors.AnsibleFilterError(
+            "Version parameter to compare against cannot be empty")
+
+    Version = LooseVersion
+    if strict:
+        Version = StrictVersion
+    elif version_type:
+        try:
+            Version = type_map[version_type]
+        except KeyError:
+            raise errors.AnsibleFilterError(
+                "Invalid version type (%s). Must be one of %s" % (
+                    version_type, ', '.join(map(repr, type_map)))
+            )
+
+    if operator in op_map:
+        operator = op_map[operator]
+    else:
+        raise errors.AnsibleFilterError(
+            'Invalid operator type (%s). Must be one of %s' % (
+                operator, ', '.join(map(repr, op_map)))
+        )
+
+    try:
+        method = getattr(py_operator, operator)
+        return method(Version(to_text(value)), Version(to_text(version)))
+    except Exception as e:
+        raise errors.AnsibleFilterError(
+            'Version comparison failed: %s' % to_native(e))
+
+
+class FilterModule(object):
+    # version comparison
+    def filters(self):
+        return {
+            'select': select,
+            'split': split_string,
+            'split_regex': split_regex,
+            'version_compare': version_compare,
+            'version': version_compare
+        }
