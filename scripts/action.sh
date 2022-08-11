c267e8c983416778ab09dad5a0820fb1b71668dc
buxiaomo <95112082@qq.com>
2022-08-11 15:39:21 +0800

    update ansible env

--- scripts/action.sh
+++ scripts/action.sh
@@ -12,20 +12,22 @@ if [ ${ARCH} != "aarch64" ] && [ ${ARCH} != "x86_64" ]; then
   echo -e "${ARCH} architecture is not supported!"
   exit 0
 fi
-ANSIBLE_ARG=""
 
 echo -e "Project name: \033[32m${PROJECT_NAME}\033[0m\tProject env: \033[32m${PROJECT_ENV}\033[0m"
 echo -e "Binary download mode: \t\t\033[32m${DOWNLOAD_WAY}\033[0m"
 echo -e "Kubernetes runtime mode: \t\033[32m${KUBE_RUNTIME}\033[0m"
 echo -e "Kubernetes version: \t\t\033[32m${KUBE_VERSION}\033[0m"
 echo -e "Kubernetes network: \t\t\033[32m${KUBE_NETWORK}\033[0m"
+
+ANSIBLE_ENV="-e PROJECT_NAME=${PROJECT_NAME} -e PROJECT_ENV=${PROJECT_ENV} -e DOWNLOAD_WAY=${DOWNLOAD_WAY} -e KUBE_VERSION=${KUBE_VERSION} -e KUBE_RUNTIME=${KUBE_RUNTIME} -e KUBE_NETWORK=${KUBE_NETWORK}"
 if [ ${REGISTRY_URL} ]; then
   echo -e "Private registry: \t\t\033[32m${REGISTRY_URL}\033[0m"
+  ANSIBLE_ENV="${ANSIBLE_ENV} -e REGISTRY_URL=${REGISTRY_URL}"
 fi
 
 if [ ${PKI_URL} ]; then
   echo -e "PKI Url: \t\t\t\033[32m${PKI_URL}\033[0m"
-  ANSIBLE_ARG="${ANSIBLE_ARG} -e PKI_URL=${PKI_URL}"
+  ANSIBLE_ENV="${ANSIBLE_ENV} -e PKI_URL=${PKI_URL}"
 fi
 
 if [ ${DOWNLOAD_WAY} == "nexus" ]; then
@@ -33,13 +35,13 @@ if [ ${DOWNLOAD_WAY} == "nexus" ]; then
     echo -e "\033[31mNexus parameter error, please set NEXUS_HTTP_USERNAME, NEXUS_HTTP_PASSWORD, NEXUS_DOMAIN_NAME, NEXUS_REPOSITORY! \033[0m "
     exit 1
   else
-    ANSIBLE_ARG="${ANSIBLE_ARG} -e NEXUS_DOMAIN_NAME=${NEXUS_DOMAIN_NAME} -e NEXUS_REPOSITORY=${NEXUS_REPOSITORY} -e NEXUS_USERNAME=${NEXUS_USERNAME} -e NEXUS_PASSWORD=${NEXUS_PASSWORD}"
     echo '-------------------------------------------------------------------------------'
     echo -e "Nexus Url: \t\t\t\033[32m${NEXUS_DOMAIN_NAME}\033[0m"
     echo -e "Nexus repository: \t\t\033[32m${NEXUS_REPOSITORY}\033[0m"
     echo -e "Nexus username: \t\t\033[32m${NEXUS_USERNAME}\033[0m"
     echo -e "Nexus password: \t\t\033[32m******\033[0m"
     echo '-------------------------------------------------------------------------------'
+    ANSIBLE_ENV="${ANSIBLE_ENV} -e NEXUS_DOMAIN_NAME=${NEXUS_DOMAIN_NAME} -e NEXUS_REPOSITORY=${NEXUS_REPOSITORY} -e NEXUS_USERNAME=${NEXUS_USERNAME} -e NEXUS_PASSWORD=${NEXUS_PASSWORD}"
   fi
 fi
 case $1 in
@@ -52,15 +54,9 @@ case $1 in
   else
     echo -e "\033[31mMessage: main branch is develop, Please do not use in production.\033[0m"
   fi
+  sleep 3
   start=$(date +%s)
-  ansible-playbook -i ./inventory/hosts ./deploy.yml \
-    -e PROJECT_NAME=${PROJECT_NAME} -e PROJECT_ENV=${PROJECT_ENV} \
-    -e DOWNLOAD_WAY=${DOWNLOAD_WAY} \
-    -e KUBE_VERSION=${KUBE_VERSION} \
-    -e KUBE_RUNTIME=${KUBE_RUNTIME} \
-    -e KUBE_NETWORK=${KUBE_NETWORK} \
-    -e REGISTRY_URL=${REGISTRY_URL} \
-    -e KUBE_ACTION="deploy" ${ANSIBLE_ARG}
+  ansible-playbook -i ./inventory/hosts ./deploy.yml -e KUBE_ACTION="deploy" ${ANSIBLE_ENV}
   if [ $? -eq 0 ];then
     end=$(date +%s)
     echo -e "\033[32mDeploy kubernetes success, execute commands is $(( end - start )) seconds, please run 'kubectl get po -A' to check the pod status.\033[0m"
@@ -71,24 +67,10 @@ case $1 in
   for host in ${SCALE_HOST_LIST_VER}; do
     sed -i "/\[worker\]/a ${host}" ./inventory/hosts
   done
-  ansible-playbook -i ./inventory/hosts ./scale.yml --limit $(echo ${SCALE_HOST_LIST_VER} | sed 's/ /,/g') \
-    -e PROJECT_NAME=${PROJECT_NAME} -e PROJECT_ENV=${PROJECT_ENV} \
-    -e DOWNLOAD_WAY=${DOWNLOAD_WAY} \
-    -e KUBE_VERSION=${KUBE_VERSION} \
-    -e KUBE_RUNTIME=${KUBE_RUNTIME} \
-    -e KUBE_NETWORK=${KUBE_NETWORK} \
-    -e REGISTRY_URL=${REGISTRY_URL} \
-    -e KUBE_ACTION="scale" ${ANSIBLE_ARG}
+  ansible-playbook -i ./inventory/hosts ./scale.yml --limit $(echo ${SCALE_HOST_LIST_VER} | sed 's/ /,/g') -e KUBE_ACTION="scale" ${ANSIBLE_ENV}
   ;;
 "upgrade")
-  ansible-playbook -i ./inventory/hosts ./upgrade.yml \
-    -e PROJECT_NAME=${PROJECT_NAME} -e PROJECT_ENV=${PROJECT_ENV} \
-    -e DOWNLOAD_WAY=${DOWNLOAD_WAY} \
-    -e KUBE_VERSION=${KUBE_VERSION} \
-    -e KUBE_RUNTIME=${KUBE_RUNTIME} \
-    -e KUBE_NETWORK=${KUBE_NETWORK} \
-    -e REGISTRY_URL=${REGISTRY_URL} \
-    -e KUBE_ACTION="upgrade" ${ANSIBLE_ARG}
+  ansible-playbook -i ./inventory/hosts ./upgrade.yml -e KUBE_ACTION="upgrade" ${ANSIBLE_ENV}
   ;;
 *) ;;
 esac
