7c7ededd6ed6e7712fb036039919b5d3c9054d1a
buxiaomo <95112082@qq.com>
2022-08-21 14:35:29 +0800

    fix

--- scripts/nexus.py
+++ scripts/nexus.py
@@ -1,337 +1,214 @@
 #!/usr/bin/env python3
 # -*- coding: UTF-8 -*-
-import re
-import IPy
-from ansible import errors
-import operator as py_operator
-from distutils.version import LooseVersion, StrictVersion
-from ansible.module_utils._text import to_native, to_text
+import argparse
+import os
+import sys
+import requests
+import json
+from version import version_map
 from distutils.version import LooseVersion
-from ansible.utils.version import SemanticVersion
 
-
-# {{ value | component_version | community.general.json_query("etcd") }}
-# pip3 install jmespath
-def component_version(value):
-    version_map = {
-        "1.14": {
-            "etcd": "3.3.10",
-            "cni": "0.7.5",
-            "runtime": {
-                "docker": "19.03.9",
-                "containerd": "1.5.11",
-                "crio": "1.14.12",
-            },
-            "plugin": {
-                "coredns": "1.3.1",
-                "metrics": "0.5.2",
-                "pause": "3.1"
-            }
-        },
-        "1.15": {
-            "etcd": "3.3.10",
-            "cni": "0.7.5",
-            "runtime": {
-                "docker": "19.03.9",
-                "containerd": "1.5.11",
-                "crio": "1.15.4",
-            },
-            "plugin": {
-                "coredns": "1.3.1",
-                "metrics": "0.5.2",
-                "pause": "3.1"
-            }
-        },
-        "1.16": {
-            "etcd": "3.3.15",
-            "cni": "0.7.5",
-            "runtime": {
-                "docker": "19.03.9",
-                "containerd": "1.5.11",
-                "crio": "1.16.6",
-            },
-            "plugin": {
-                "coredns": "1.6.2",
-                "metrics": "0.5.2",
-                "pause": "3.1"
-            }
-        },
-        "1.17": {
-            "etcd": "3.4.3",
-            "cni": "0.7.5",
-            "runtime": {
-                "docker": "19.03.9",
-                "containerd": "1.5.11",
-                "crio": "1.17.5",
-            },
-            "plugin": {
-                "coredns": "1.6.5",
-                "metrics": "0.5.2",
-                "pause": "3.1"
-            }
-        },
-        "1.18": {
-            "etcd": "3.4.3",
-            "cni": "0.8.5",
-            "runtime": {
-                "docker": "19.03.9",
-                "containerd": "1.5.11",
-                "crio": "1.18.6",
-            },
-            "plugin": {
-                "coredns": "1.6.7",
-                "metrics": "0.5.2",
-                "pause": "3.2"
-            }
-        },
-        "1.19": {
-            "etcd": "3.4.13",
-            "cni": "0.8.6",
-            "runtime": {
-                "docker": "19.03.9",
-                "containerd": "1.5.11",
-                "crio": "1.19.6",
-            },
-            "plugin": {
-                "coredns": "1.7.0",
-                "metrics": "0.5.2",
-                "pause": "3.2"
-            }
-        },
-        "1.20": {
-            "etcd": "3.4.13",
-            "cni": "0.8.7",
-            "runtime": {
-                "docker": "19.03.9",
-                "containerd": "1.5.0",
-                "crio": "1.20.9",
-            },
-            "plugin": {
-                "coredns": "1.7.0",
-                "metrics": "0.6.1",
-                "pause": "3.2"
-            }
-        },
-        "1.21": {
-            "etcd": "3.4.13",
-            "cni": "0.9.1",
-            "runtime": {
-                "docker": "20.10.9",
-                "containerd": "1.5.0",
-                "crio": "1.21.7",
-            },
-            "plugin": {
-                "coredns": "1.8.0",
-                "metrics": "0.6.1",
-                "pause": "3.4.1"
-            }
-        },
-        "1.22": {
-            "etcd": "3.5.0",
-            "cni": "0.9.1",
-            "runtime": {
-                "docker": "20.10.9",
-                "containerd": "1.5.13",
-                "crio": "1.22.5",
-            },
-            "plugin": {
-                "coredns": "1.8.4",
-                "metrics": "0.6.1",
-                "pause": "3.5"
-            }
-        },
-        "1.23": {
-            "etcd": "3.5.1",
-            "cni": "0.9.1",
-            "runtime": {
-                "docker": "20.10.9",
-                "containerd": "1.6.0",
-                "crio": "1.23.3",
-            },
-            "plugin": {
-                "coredns": "1.8.6",
-                "metrics": "0.6.1",
-                "pause": "3.6"
-            }
-        },
-        "1.24": {
-            "etcd": "3.5.3",
-            "cni": "0.9.1",
-            "runtime": {
-                "crio": "1.24.2",
-                "containerd": "1.6.8",
-            },
-            "plugin": {
-                "coredns": "1.8.6",
-                "metrics": "0.6.1",
-                "pause": "3.6"
-            }
-        }
-    }
-
-    if version_map.get(value) is not None:
-        return version_map.get(value)
-    if version_map.get(value[:4]) is None:
-        raise errors.AnsibleFilterError('kubernetes version not supported')
-    return version_map.get(value[:4])
-
-
-# {{ value | ip }}
-# return 4 or 6
-def ip(value):
+def Download(url, path, quiet):
+    response = requests.get(url, stream=True)
+    size = 0
+    chunk_size = 1024
+    content_size = int(response.headers['content-length'])
     try:
-        return IPy.IP(value).version()
-    except Exception as e:
-        raise errors.AnsibleFilterError(
-            'ip failed: %s' % to_native(e))
-
-
-# {{ value | ip_format }}
-# if value is ipv4, will return ipv4. if value is ipv6 will return [value]
-def ip_format(value):
-    try:
-        version = IPy.IP(value).version()
-        if version == 4:
-            return value
+        if response.status_code == 200:
+            print('=> start download {name} {size:.2f} MB'.format(
+                name=os.path.basename(path), size=content_size / chunk_size / 1024)
+            )
+            with open(path, 'wb') as file:
+                for data in response.iter_content(chunk_size=chunk_size):
+                    file.write(data)
+                    size += len(data)
+                    if not quiet:
+                        print('\r' + '%s %.2f%%' % (
+                            '*' * int(size * 50 / content_size), float(size / content_size * 100)), end=' ')
+            print()
+            return True
         else:
-            return "[%s]" % value
+            raise Exception(response.status_code)
     except Exception as e:
-        raise errors.AnsibleFilterError(
-            'ip failed: %s' % to_native(e))
+        print("Exception occurs in Downloading, status code: %s..." % e)
+        return False
 
 
-def interception(value, x, y):
-    return value[x:len(value) - y]
+class Nexus:
+    def __init__(self, url, repository, username, password):
+        self.url = url
+        self.repository = repository
+        self.username = username
+        self.password = password
 
-
-# {{ value | select('eq', '4', true, false) }}
-# Ternary expression
-def select(value, operator='eq', expectations=None, tValue=None, fValue=None):
-    if not value:
-        raise errors.AnsibleFilterError("Input value cannot be empty")
-    if not expectations:
-        raise errors.AnsibleFilterError("Input expectations cannot be empty")
-
-    op_map = {
-        '==': 'eq', '=': 'eq', 'eq': 'eq',
-        '<': 'lt', 'lt': 'lt',
-        '<=': 'le', 'le': 'le',
-        '>': 'gt', 'gt': 'gt',
-        '>=': 'ge', 'ge': 'ge',
-        '!=': 'ne', '<>': 'ne', 'ne': 'ne'
-    }
-    if operator in op_map:
-        operator = op_map[operator]
-    else:
-        raise errors.AnsibleFilterError(
-            'Invalid operator type (%s). Must be one of %s' % (
-                operator, ', '.join(map(repr, op_map)))
-        )
-
-    try:
-        method = getattr(py_operator, operator)
-        if method(to_text(value), to_text(expectations)):
-            return tValue
+    def Upload(self, src, directory):
+        url = '%s/repository/%s' % (self.url, self.repository)
+        resp = None
+        content = open(src, 'rb').read()
+        if self.username is not None and self.password is not None:
+            auth = (self.username, self.password)
+            resp = requests.put(
+                "%s/%s/%s" % (url, directory, os.path.basename(src)), data=content, auth=auth)
         else:
-            return fValue
-    except Exception as e:
-        raise errors.AnsibleFilterError(
-            'Version comparison failed: %s' % to_native(e))
+            resp = requests.put(
+                "%s/%s/%s" % (url, directory, os.path.basename(src)), data=content)
+        if resp.status_code != 201:
+            print("Upload failed, status code: %d" % resp.status_code)
+            return False
+        else:
+            print("Upload success.")
+            return True
+
+    def Auth(self):
+        url = '%s/service/rest/v1/status/check' % (self.url)
+        resp = None
+        if self.username is not None and self.password is not None:
+            auth = (self.username, self.password)
+            resp = requests.get(url, auth=auth)
+        else:
+            resp = requests.get(url)
+
+        if resp.status_code != 200:
+            return False
+        else:
+            return True
 
 
-def split_string(string, separator=' '):
-    try:
-        return string.split(separator)
-    except Exception as e:
-        raise errors.AnsibleFilterError(
-            'split plugin error: %s, provided string: "%s"' % str(e), str(string))
+def downloadToLocal(arg):
+    kubeVersion = arg.kubernetes[:4]
 
+    basePath = os.path.split(os.path.realpath(__file__))[0]
+    basePath = basePath + "/src/" + arg.kubernetes
+    if not os.path.exists(basePath):
+        os.makedirs(basePath)
 
-def split_regex(string, separator_pattern='\s+'):
-    try:
-        return re.split(separator_pattern, string)
-    except Exception as e:
-        raise errors.AnsibleFilterError(
-            'split plugin error: %s, provided string: "%s"' % str(e), str(string))
-
-
-# {{ value | version("0.8.0",">=") }}
-def version_compare(value, version, operator='eq', strict=None, version_type=None):
-    ''' Perform a version comparison on a value '''
-    op_map = {
-        '==': 'eq', '=': 'eq', 'eq': 'eq',
-        '<': 'lt', 'lt': 'lt',
-        '<=': 'le', 'le': 'le',
-        '>': 'gt', 'gt': 'gt',
-        '>=': 'ge', 'ge': 'ge',
-        '!=': 'ne', '<>': 'ne', 'ne': 'ne'
-    }
-
-    type_map = {
-        'loose': LooseVersion,
-        'strict': StrictVersion,
-        'semver': SemanticVersion,
-        'semantic': SemanticVersion,
-    }
-
-    if strict is not None and version_type is not None:
-        raise errors.AnsibleFilterError(
-            "Cannot specify both 'strict' and 'version_type'")
-
-    if not value:
-        raise errors.AnsibleFilterError("Input version value cannot be empty")
-
-    if not version:
-        raise errors.AnsibleFilterError(
-            "Version parameter to compare against cannot be empty")
-
-    Version = LooseVersion
-    if strict:
-        Version = StrictVersion
-    elif version_type:
-        try:
-            Version = type_map[version_type]
-        except KeyError:
-            raise errors.AnsibleFilterError(
-                "Invalid version type (%s). Must be one of %s" % (
-                    version_type, ', '.join(map(repr, type_map)))
+    jsonFile = []
+    version = version_map.get(kubeVersion).get('runtime').get('docker')
+    if version is not None:
+        url = "https://download.docker.com/linux/static/stable/x86_64/docker-%s.tgz" % version
+        if Download(url, "%s/docker-%s.tgz" % (basePath, version), arg.quiet):
+            jsonFile.append(
+                {
+                    'src': "./scripts/src/%s/docker-%s.tgz" % (arg.kubernetes, version),
+                    'dest': "/linux/static/stable/x86_64"
+                }
             )
 
-    if operator in op_map:
-        operator = op_map[operator]
-    else:
-        raise errors.AnsibleFilterError(
-            'Invalid operator type (%s). Must be one of %s' % (
-                operator, ', '.join(map(repr, op_map)))
-        )
+    version = version_map.get(kubeVersion).get('runtime').get('crio')
+    if version is not None:
+        url = "https://github.com/cri-o/cri-o/releases/download/v%s/cri-o.amd64.v%s.tar.gz" % (version, version)
+        if Download(url, "%s/cri-o.amd64.v%s.tar.gz" % (basePath, version), arg.quiet):
+            jsonFile.append(
+                {
+                    'src': "./scripts/src/%s/cri-o.amd64.v%s.tar.gz" % (arg.kubernetes, version),
+                    'dest': "/cri-o/cri-o/releases/download/v%s" % version
+                }
+            )
+
+    version = version_map.get(kubeVersion).get('runtime').get('containerd')
+    if version is not None:
+        
+        url = "https://github.com/containerd/containerd/releases/download/v%s/cri-containerd-%s-linux-amd64.tar.gz" % (
+            version, version)
+        if Download(url, "%s/cri-containerd-%s-linux-amd64.tar.gz" % (basePath, version), arg.quiet):
+            jsonFile.append(
+                {
+                    'src': "./scripts/src/%s/cri-containerd-%s-linux-amd64.tar.gz" % (arg.kubernetes, version),
+                    'dest': "/containerd/containerd/releases/download/v%s" % version
+                }
+            )
+
+    version = version_map.get(kubeVersion).get('etcd')
+    if version is not None:
+        url = "https://github.com/coreos/etcd/releases/download/v%s/etcd-v%s-linux-amd64.tar.gz" % (version, version)
+        if Download(url, "%s/etcd-v%s-linux-amd64.tar.gz" % (basePath, version), arg.quiet):
+            jsonFile.append(
+                {
+                    'src': "./scripts/src/%s/etcd-v%s-linux-amd64.tar.gz" % (arg.kubernetes, version),
+                    'dest': "/coreos/etcd/releases/download/v%s" % version
+                }
+            )
+
+    version = version_map.get(kubeVersion).get('cni')
+    if version is not None:
+        if version >= 0.8.0
+        url = "https://github.com/containernetworking/plugins/releases/download/v%s/cni-plugins-linux-amd64-v%s.tgz" % (
+            version, version)
+        if Download(url, "%s/cni-plugins-linux-amd64-v%s.tgz" % (basePath, version), arg.quiet):
+            jsonFile.append(
+                {
+                    'src': "./scripts/src/%s/cni-plugins-linux-amd64-v%s.tgz" % (arg.kubernetes, version),
+                    'dest': "/containernetworking/plugins/releases/download/v%s" % version
+                }
+            )
+
+
+    file_list = [
+        "kube-apiserver",
+        "kube-controller-manager",
+        "kube-scheduler",
+        "kubectl",
+        "kube-proxy",
+        "kubelet",
+    ]
+    for f in file_list:
+        url = "https://storage.googleapis.com/kubernetes-release/release/v%s/bin/linux/amd64/%s" % (
+            arg.kubernetes, f)
+        if Download(url, "%s/%s" % (basePath, f), arg.quiet):
+            jsonFile.append(
+                {
+                    'src': "./scripts/src/%s/%s" % (arg.kubernetes, f),
+                    'dest': "/kubernetes-release/release/v%s/bin/linux/amd64" % arg.kubernetes
+                }
+            )
+
+
+
+    with open("%s/%s.json" % (basePath, arg.kubernetes), "w") as f:
+        json.dump(jsonFile, f)
+
+    print("files is save on src directory")
+
+
+def uploadToNexus(arg):
+    basePath = os.path.split(os.path.realpath(__file__))[0]
+    basePath = basePath + "/src/" + arg.kubernetes
+
+    nexus = Nexus(arg.url, arg.repository, arg.username, arg.password)
+    if not nexus.Auth():
+        print("Username or Password is error!")
+        sys.exit(2)
+    print("Upload package to nexus...")
+    with open("%s/%s.json" % (basePath, arg.kubernetes), "r") as f:
+        for item in json.load(f):
+            nexus.Upload(src=item.get('src'), directory=item.get('dest'))
+    sys.exit(0)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Download package and upload to nexus.')
+    subparsers = parser.add_subparsers(
+        title='commands',
+        description='download or upload',
+        help='download or upload'
+    )
+    download = subparsers.add_parser('download')
+    download.add_argument('--quiet', action='store_true',  help='silent download')
+    download.add_argument('--kubernetes', metavar='kubernetes', required=True, help='kubernetes version')
+    download.set_defaults(func=downloadToLocal)
+
+    upload = subparsers.add_parser('upload')
+    upload.add_argument('--kubernetes', metavar='kubernetes', required=True, help='kubernetes version')
+    upload.add_argument('--url', metavar='url', help='nexus url', required=True)
+    upload.add_argument('--repository', metavar='repository', help='nexus repository name', required=True)
+    upload.add_argument('--username', default=None, metavar='username', help='nexus username', required=True)
+    upload.add_argument('--password', default=None, metavar='password', help='nexus password', required=True)
+    upload.set_defaults(func=uploadToNexus)
+
+    args = parser.parse_args()
 
     try:
-        method = getattr(py_operator, operator)
-        return method(Version(to_text(value)), Version(to_text(version)))
-    except Exception as e:
-        raise errors.AnsibleFilterError(
-            'Version comparison failed: %s' % to_native(e))
-
-
-class FilterModule(object):
-    def filters(self):
-        return {
-            'select': select,
-            'split': split_string,
-            'split_regex': split_regex,
-            'version_compare': version_compare,
-            'version': version_compare,
-            'interception': interception,
-            'ip_format': ip_format,
-            'ip': ip,
-            'component_version': component_version
-        }
-
-
-# if __name__ == '__main__':
-#     component_version("1.23.7")
-#     print(select("4", "eq", "4", "10.96.0.0/12","fd74:ca9b:0172:0019::/110"))
-#     a = "[fd74:ca9b:0172:0018::/64]"
-#     print(a[1:len(a) - 1])
-#     print()
-#     print(version_compare("0.7.5", "0.8.0", ">="))
+        args.func(args)
+    except AttributeError:
+        parser.print_help()
+        parser.exit()
